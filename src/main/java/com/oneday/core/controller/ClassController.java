92312c965741d4ce99f358801b92459afecf4acd
pkh <khp9800@hansung.ac.kr>
2025-11-06 23:27:19 +0900

    feat: add find class

---
+++ src/main/java/com/oneday/core/controller/ClassController.java
@@ -0,0 +1,34 @@
+package com.oneday.core.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import com.oneday.core.model.Classes;
+import com.oneday.core.service.ClassService;
+
+@RestController
+@RequestMapping("/api/classes")
+public class ClassController {
+
+	private final ClassService classService;
+
+	@Autowired
+	public ClassController(ClassService classService) {
+		this.classService = classService;
+	}
+
+	@GetMapping
+	public List<Classes> getAllClasses() {
+		return classService.getAllClasses();
+	}
+
+	@GetMapping("/{classId}")
+	public Classes getClassById(@PathVariable int classId) {
+		return classService.getClassById(classId);
+	}
+}
---
+++ src/main/java/com/oneday/core/model/Classes.java
@@ -0,0 +1,63 @@
+package com.oneday.core.model;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "classes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Classes {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "class_id")
+	private int classId;
+
+	@Column(name = "teacher_id")
+	private int teacherId;
+
+	@Column(name = "category_id")
+	private int categoryId;
+
+	@Column(name = "class_name")
+	private String className;
+
+	@Column(name = "class_detail")
+	private String classDetail;
+
+	@Column(name = "start_at")
+	private LocalDateTime startAt;
+
+	@Column(name = "end_at")
+	private LocalDateTime endAt;
+
+	@Column(name = "longitude")
+	private String longitude;
+
+	@Column(name = "latitude")
+	private String latitude;
+
+	@Column(name = "location")
+	private String location;
+
+	@Column(name = "zipcode")
+	private String zipcode;
+
+	@Column(name = "max_capacity")
+	private int maxCapacity;
+
+	@Column(name = "price")
+	private int price;
+}
