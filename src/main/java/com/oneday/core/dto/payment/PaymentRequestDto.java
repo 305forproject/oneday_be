81afe2948a09f8c614c0efb0a0ba9b26efca623a
kyeonghwi <kyeonghwi@gmail.com>
2025-11-19 15:12:17 +0900

    feat: get toss response

--- src/main/java/com/oneday/core/dto/payment/PaymentRequestDto.java
+++ src/main/java/com/oneday/core/dto/payment/PaymentRequestDto.java
@@ -1,18 +1,15 @@
 package com.oneday.core.dto.payment;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 
-import java.util.Map;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class PaymentRequestDto {
-
-	@NotNull
-	@Positive
-	private int timeId;
-
-	@NotNull
-	private Map<String, Object> tossResponse;
+	private int timeId;        // 예약할 시간 ID
+	private String paymentKey; // 토스 결제 키
+	private String orderId;    // 주문 ID
+	private long amount;       // 결제 금액
 }
